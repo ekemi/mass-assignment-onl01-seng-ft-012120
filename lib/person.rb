5a8a921d20b80fb684cd250de8bbf83884f270ba
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-02-06 21:26:42 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -13,16 +13,27 @@ wrist_size:, glove_size:, pant_length:, pant_width: )
     @name =  name
     
     @birthday = birthday 
-    @hair_color= hair_color 
+    
+    @hair_color= hair_color
+    
     @eye_color= eye_color 
+    
     @height=height 
+    
     @weight=weight 
+    
     @handed = handed 
+    
     @complexion = complexion 
+    
     @t_shirt_size = t_shirt_size 
+    
     @wrist_size = wrist_size
+    
     @glove_size=glove_size 
+    
     @pant_length = pant_length 
+    
     @pant_width = pant_width
 
   end
